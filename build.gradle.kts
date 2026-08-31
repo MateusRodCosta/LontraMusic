fc69af54beae89f0db9dcabf285c358976313917
Mateus Rodrigues Costa <mateusrodcosta@gmail.com>
2026-08-31 15:15:05 -0300

    Setup dependency analysis plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    alias(libs.plugins.dependency.analysis)
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.aboutlibraries.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     id("com.ncorti.ktfmt.gradle") version "0.27.0" apply false
+}
+
+dependencyAnalysis {
+    issues {
+        all {
+            onAny {
+                severity("fail")
+            }
+        }
+    }
 }
